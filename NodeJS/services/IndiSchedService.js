4cea5303f526980d8aea13ec7ef37adda2946d56
driedchpstck <driedchapstick@gmail.com>
2021-02-18 21:09:54 -0500

    Frontend and Backend for modifying a Schedule.

---
+++ NodeJS/services/IndiSchedService.js
@@ -0,0 +1,69 @@
+const sql = require("mssql");
+const config = require("./dbconfig");
+
+async function InitialLoad(SchedID) {
+  try {
+    let pool = await sql.connect(config);
+    let SchedData = await pool.request().input("SchedID", sql.Int, SchedID).execute("GetSpecificSchedule");
+    let Intervals = await pool.request().execute("GetAllIntervals");
+    let SchedsComps = await pool.request().input("SchedID", sql.Int, SchedID).execute("GetSchedsComps");
+    return [SchedData.recordset, Intervals.recordset, SchedsComps.recordset];
+  } catch (error) {
+    console.log("============ERROR============");
+    console.log("Error in IndiSchedService.js");
+    console.log("============ERROR============");
+    console.log(error);
+    console.log("");
+  }
+}
+
+//For SchedID and InterID
+function CheckID(input) {
+  let reggie = new RegExp("^([0-9]{1,})$");
+  if (reggie.test(input) == false) {
+    return "ThisFailedSoWeAreSupplyingThisValue";
+  }else{
+    return input;
+  }
+  
+}
+function CheckSchedName(input) {
+  let reggie = new RegExp("^([a-zA-Z0-9_\\-\\.\\/\\\ ]{1,20})$");
+  if (reggie.test(input) == false) {
+    return "ThisFailedSoWeAreSupplyingThisValue";
+  }else{
+    return input;
+  }
+  
+}
+function CheckBaseTime(input) {
+  let reggie = new RegExp("^([0-9]{1,4})$");
+  if (reggie.test(input) == false) {
+    return "ThisFailedSoWeAreSupplyingThisValue";
+  }else{
+    return input;
+  }
+  
+}
+
+async function UpdateSchedule(SchedID, SchedName, BaseTime, InterID) {
+  SchedID = CheckID(SchedID);
+  SchedName = CheckSchedName(SchedName);
+  BaseTime = CheckBaseTime(BaseTime);
+  InterID = CheckID(InterID);
+
+  if(SchedID != "ThisFailedSoWeAreSupplyingThisValue" && SchedName != "ThisFailedSoWeAreSupplyingThisValue" && BaseTime != "ThisFailedSoWeAreSupplyingThisValue" && InterID != "ThisFailedSoWeAreSupplyingThisValue"){
+    let pool = await sql.connect(config);
+    await pool.request()
+      .input("SchedID", sql.Int, SchedID)
+      .input("SchedName", sql.NVarChar, SchedName)
+      .input("BaseTime", sql.TinyInt, BaseTime)
+      .input("InterID", sql.Int, InterID)
+      .execute("UpdateSchedule");
+  }
+}
+
+module.exports = {
+  InitialLoad: InitialLoad,
+  UpdateSchedule: UpdateSchedule,
+};
